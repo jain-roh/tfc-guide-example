74928aaf0f879ab3d05ba70a4faa0770d0d0680f
Rohit Jain <34594228+jain-roh@users.noreply.github.com>
2023-02-10 13:23:13 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,11 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.28.0"
     }
-    backend "s3" {
-      bucket = "evself"
-      key    = "lambda-api"
-      region = "us-west-1"
-    }
     random = {
       source  = "hashicorp/random"
       version = "3.0.0"
